6e60122478769580ac3be1fc45d0fff731823013
kzmeyao <kzmeyao@gmail.com>
2019-02-03 19:56:35 -0800

    refactor: use single quotes

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,29 @@
-import { Link } from "gatsby";
-import PropTypes from "prop-types";
-import React from "react";
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`
+      background: '#ffffff',
+      marginBottom: '1.45rem'
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
+        margin: '0 auto',
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`
+        padding: '1rem'
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <Link
+        to='/'
+        style={{
+          textDecoration: 'none'
+        }}
+      >
+        {siteTitle}
+      </Link>
     </div>
   </header>
 );
@@ -36,7 +33,7 @@ Header.propTypes = {
 };
 
 Header.defaultProps = {
-  siteTitle: ``
+  siteTitle: ''
 };
 
 export default Header;
