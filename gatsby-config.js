da1b4b521ef178d719c5a9ce5d4f95cb92c773b3
kzmeyao <kzmeyao@gmail.com>
2019-02-04 20:14:05 -0800

    feat: add cardo and lato font

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,21 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
       }
+    },
+    {
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
+      options: {
+        fonts: [
+          {
+            family: 'Cardo',
+            variants: ['400', '700']
+          },
+          {
+            family: 'Lato',
+            variants: ['400', '400i', '700']
+          }
+        ]
+      }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
