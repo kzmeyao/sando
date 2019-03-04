c525e225bc0549fb1d79afcd53e82b15442e6646
kzmeyao <kzmeyao@gmail.com>
2019-03-03 21:25:35 -0800

    feat: make entire area clickable

--- src/components/posts/internals/post-card.js
+++ src/components/posts/internals/post-card.js
@@ -7,15 +7,17 @@ const PostCard = ({ post }) => {
   return (
     <div className="post-card">
       <Link to={path}>
-        <img
-          src={`https://res.cloudinary.com/sando/image/upload/w_600/${heroImage}`}
-        />
-      </Link>
-      <div className="subtitle">{regionHierarchy.toUpperCase()}</div>
-      <h3>{place}</h3>
-      <p>{excerpt}</p>
-      <Link className="read-more" to={path}>
-        Read more
+        <>
+          <img
+            src={`https://res.cloudinary.com/sando/image/upload/w_600/${heroImage}`}
+          />
+          <div className="subtitle">{regionHierarchy.toUpperCase()}</div>
+          <h3>{place}</h3>
+          <p>{excerpt}</p>
+          <div className="read-more" to={path}>
+            Read more
+          </div>
+        </>
       </Link>
     </div>
   );
