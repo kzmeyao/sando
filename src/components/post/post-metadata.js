df5a188f119c6456bd59f66306602e1a27144bf4
kzmeyao <kzmeyao@gmail.com>
2019-03-15 22:29:50 -0700

    feat: copy url

--- src/components/post/post-metadata.js
+++ src/components/post/post-metadata.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './post-metadata.css';
 
-const PostMetadata = ({ date, photoGear }) => (
-  <div className="post-metadata">
-    <dl>
-      <dt>Year visited</dt>
-      <dd>{date.split('-')[0]}</dd>
-      <dt>Photo gear used</dt>
-      <dd>{photoGear}</dd>
-      <dt>Share</dt>
-      <dd>Twitter, Facebook, Copy URL</dd>
-    </dl>
-  </div>
-);
+const PostMetadata = ({ date, photoGear }) => {
+  const [copied, setCopied] = useState(false);
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setCopied(true);
+  };
+
+  return (
+    <div className="post-metadata">
+      <dl>
+        <dt>Year visited</dt>
+        <dd>{date.split('-')[0]}</dd>
+        <dt>Photo gear used</dt>
+        <dd>{photoGear}</dd>
+        <dt>Share</dt>
+        <dd>
+          <div onClick={copyToClipboard}>
+            {copied ? 'Copied!' : <span className="copy-link">Copy URL</span>}
+          </div>
+        </dd>
+      </dl>
+    </div>
+  );
+};
 
 export default PostMetadata;
