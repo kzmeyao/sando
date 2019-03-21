5ce89f69126247f098e6dac8cff0fd74a7f5b9c2
kzmeyao <kzmeyao@gmail.com>
2019-03-20 22:57:59 -0700

    feat: better about page

---
+++ src/components/about/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './internals/about.css';
+
+const About = () => (
+  <div className="about">
+    <h4>About the website:</h4>
+    <p>
+      In Japan, `sando` is short for sandwich. Also in Japan, I joined the cult
+      of the humble egg salad sando after just one bite. Its simplicity, taste,
+      and value just can't be beat. It's probably not even a stretch to say that
+      my love for travel came after having that said bite. So here I am, writing
+      this travel journal / travel guide, and I can't think of a better name for
+      it than to name it after the perfect food.
+    </p>
+    <br />
+    <h4>About the author:</h4>
+    <p>
+      I'm a software engineer by trade, but I'm also a serial dabbler. My
+      curiosity often leads me down rabbitholes and I spend an inordinate amount
+      of time learning and doing something until I find something new to take
+      its place. For example, I've composed songs while learning to play guitar,
+      made the first scene of a JRPG while learning how to do pixel art, built a
+      digital 3d model of a house while dreaming of eventually designing my own
+      house. Even building this site happened on a whim, but who knows, maybe I
+      will never get tired of photography and continue this project
+      indefinitely.
+    </p>
+  </div>
+);
+
+export default About;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import { HomeHeader } from '../components/header';
+import About from '../components/about';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
@@ -9,14 +9,7 @@ const AboutPage = () => (
   <Layout header={<HomeHeader title="sando" />}>
     <SEO title="About" />
     <div className="pure-u-padding-one">
-      <h3>The website:</h3>
-      <p>
-        The humble egg salad sando (short for sandwich). So simple yet so
-        delicious.
-      </p>
-      <br />
-      <h3>The author:</h3>
-      <p>Software engineer by trade, but I'm also a serial dabbler.</p>
+      <About />
     </div>
   </Layout>
 );
