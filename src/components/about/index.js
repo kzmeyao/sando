683a1d55a5e8dec4fcf3158d75413d28d3005552
kzmeyao <kzmeyao@gmail.com>
2019-03-21 20:41:41 -0700

    refactor: words

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -5,25 +5,25 @@ const About = () => (
   <div className="about">
     <h4>About the website:</h4>
     <p>
-      In Japan, `sando` is short for sandwich. Also in Japan, I joined the cult
-      of the humble egg salad sando after just one bite. Its simplicity, taste,
-      and value just can't be beat. It's probably not even a stretch to say that
-      my love for travel came after having that said bite. So here I am, writing
-      this travel journal / travel guide, and I can't think of a better name for
-      it than to name it after the perfect food.
+      On my first trip to Japan, I ended up joining the cult of the humble egg
+      salad sando after just one bite. For the uninitiated, `sando` is short for
+      sandwich, and the egg salad sando is the perfect sandwich. Its simplicity,
+      taste, and value just can't be beat. It's probably not even a stretch to
+      say that my love for travel came after having that said bite. So here I
+      am, writing this travel journal / travel guide, and I can't think of a
+      better name for it than to name it after the thing that started it all.
     </p>
     <br />
     <h4>About the author:</h4>
     <p>
       I'm a software engineer by trade, but I'm also a serial dabbler. My
-      curiosity often leads me down rabbitholes and I spend an inordinate amount
-      of time learning and doing something until I find something new to take
-      its place. For example, I've composed songs while learning to play guitar,
-      made the first scene of a JRPG while learning how to do pixel art, built a
-      digital 3d model of a house while dreaming of eventually designing my own
-      house. Even building this site happened on a whim, but who knows, maybe I
-      will never get tired of photography and continue this project
-      indefinitely.
+      curiosity often leads me down creative rabbitholes and I spend an
+      inordinate amount of time learning and making something until I find
+      another new thing to take its place. For example, I've composed songs
+      while learning to play guitar, made the first scene of a JRPG while
+      learning how to draw pixel art, 3d-modeled a house while dreaming of
+      eventually designing my own house. Come to think of it, even building this
+      site happened on a whim - here's hoping that it'll last.
     </p>
   </div>
 );
