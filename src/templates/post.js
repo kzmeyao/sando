8c7bcc10ff34946dc85a4de7882fa204c3d2baa5
kzmeyao <kzmeyao@gmail.com>
2019-03-23 10:53:51 -0700

    fix: post spacing in tablet

--- src/templates/post.js
+++ src/templates/post.js
@@ -22,11 +22,11 @@ export default function Template({ data }) {
     <Layout header={<PostHeader title={place} subtitle={regionHierarchy} />}>
       <Helmet title={`${place} | sando`} />
       <div className="flex flex-wrap pt-4">
-        <div className="md:w-2/5 md:pr-8">
+        <div className="md:w-1/2 lg:w-2/5 md:pr-8">
           <PostText html={post.html} />
           <PostMetadata date={date} photoGear={photoGear} />
         </div>
-        <div className="md:w-3/5">
+        <div className="md:w-1/2 lg:w-3/5">
           <Gallery imagePrefix={imagePrefix} images={images} />
         </div>
       </div>
