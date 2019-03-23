a0e98f3dba025dbb548c25df357f753b655c59f3
kzmeyao <kzmeyao@gmail.com>
2019-03-23 14:23:39 -0700

    fix: justify for larger screens

--- src/components/posts/internals/filtered-posts.js
+++ src/components/posts/internals/filtered-posts.js
@@ -3,7 +3,7 @@ import PostCard from './post-card';
 
 const FilteredPosts = ({ posts }) => (
   <div className="filtered-posts">
-    <ul className="flex flex-wrap justify-center list-reset pt-5">
+    <ul className="flex flex-wrap justify-center md:justify-start list-reset pt-5">
       {posts.map(post => (
         <li className="w-full md:w-1/2 mb-6 max-w-sm" key={post.place}>
           <PostCard post={post} />
