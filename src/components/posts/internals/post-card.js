8b26a59d5e4efd9ddc09ea92a14be0847afea6f1
kzmeyao <kzmeyao@gmail.com>
2019-05-27 15:30:20 -0700

    feat: make hover more apparent

--- src/components/posts/internals/post-card.js
+++ src/components/posts/internals/post-card.js
@@ -9,7 +9,9 @@ const PostCard = ({ post }) => {
         <img
           src={`https://res.cloudinary.com/sando/image/upload/w_600/${heroImage}`}
         />
-        <div className="text-xxs pt-3">{regionHierarchy.toUpperCase()}</div>
+        <div className="group-hover:underline text-xxs pt-3">
+          {regionHierarchy.toUpperCase()}
+        </div>
         <h2 className="font-heading pt-3 text-1xl">{place}</h2>
         <p className="pt-3 leading-normal text-sm">{excerpt}</p>
         <div className="italic group-hover:underline pt-3 text-xs text-grey-dark">
