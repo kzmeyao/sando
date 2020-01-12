0df3d6932fd7ae709a9617dd14d4bb910cc4d4fd
kzmeyao <kzmeyao@gmail.com>
2020-01-11 17:42:00 -0800

    switch post card to lazy

--- src/components/posts/internals/post-card.js
+++ src/components/posts/internals/post-card.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import LazyImage from '../../common/LazyImage';
 
 const PostCard = ({ post }) => {
   const { excerpt, heroImage, path, place, regionHierarchy } = post;
   return (
     <div className="group">
       <Link className="no-underline text-grey-darkest" to={`${path}`}>
-        <img
-          src={`https://res.cloudinary.com/sando/image/upload/w_600/${heroImage}`}
-        />
+        <LazyImage relSrc={`w_600/${heroImage}`} />
         <div className="group-hover:underline text-xxs pt-3">
           {regionHierarchy.toUpperCase()}
         </div>
