2e14af0b98e01fd7f87a20b3dd35ffc5e3a9e487
kzmeyao <kzmeyao@gmail.com>
2020-01-11 17:11:50 -0800

    fix spacing on post cards

--- src/components/posts/internals/post-card.js
+++ src/components/posts/internals/post-card.js
@@ -12,9 +12,9 @@ const PostCard = ({ post }) => {
         <div className="group-hover:underline text-xxs pt-3">
           {regionHierarchy.toUpperCase()}
         </div>
-        <h2 className="font-heading font-semibold pt-3 text-2xl">{place}</h2>
-        <p className="pt-3 leading-normal text-sm">{excerpt}</p>
-        <div className="italic group-hover:underline pt-3 text-xs text-grey-dark">
+        <h2 className="font-heading font-semibold pt-1 text-2xl">{place}</h2>
+        <p className="leading-normal pt-1 text-sm">{excerpt}</p>
+        <div className="italic group-hover:underline pt-2 text-xs text-grey-dark">
           Read more
         </div>
       </Link>
