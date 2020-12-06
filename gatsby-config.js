9f91c79fb731cbcc50e7ee3c1c9fa6dc33425e48
kzmeyao <kzmeyao@gmail.com>
2020-12-05 16:27:27 -0800

    swap out font plugin and add tailwind deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     author: 'Kevin Yao',
     description: '',
-    title: 'sando'
+    title: 'sando',
   },
   plugins: [
     'gatsby-plugin-resolve-src',
@@ -11,21 +11,21 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-polyfill-io',
       options: {
-        features: ['IntersectionObserver']
-      }
+        features: ['IntersectionObserver'],
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'places',
-        path: `${__dirname}/src/places`
-      }
+        path: `${__dirname}/src/places`,
+      },
     },
     {
       resolve: 'gatsby-transformer-remark',
       options: {
         // plugins: ['gatsby-plugin-catch-links', 'gatsby-plugin-react-helmet']
-      }
+      },
     },
     {
       resolve: 'gatsby-plugin-manifest',
@@ -36,34 +36,26 @@ module.exports = {
         background_color: '#333333',
         theme_color: '#333333',
         display: 'minimal-ui',
-        icon: 'src/images/sando-icon.png' // This path is relative to the root of the site.
-      }
+        icon: 'src/images/sando-icon.png', // This path is relative to the root of the site.
+      },
     },
     {
-      resolve: 'gatsby-plugin-prefetch-google-fonts',
+      resolve: 'gatsby-plugin-google-fonts',
       options: {
-        fonts: [
-          {
-            family: 'Cardo',
-            variants: ['400', '700']
-          },
-          {
-            family: 'Lato',
-            variants: ['400', '400i', '700']
-          }
-        ]
-      }
+        fonts: ['cardo:400,700', 'lato:400,400i,700'],
+        display: 'swap',
+      },
     },
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
         trackingId: 'UA-140891664-1',
         head: false,
-        anonymize: true
-      }
-    }
+        anonymize: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ]
+  ],
 };
