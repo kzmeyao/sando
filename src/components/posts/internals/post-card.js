28dd3309f10eed27da48b4d0fe5c6b97cf6485ef
kzmeyao <kzmeyao@gmail.com>
2021-05-15 16:35:54 -0700

    add years to subtitle

--- src/components/posts/internals/post-card.js
+++ src/components/posts/internals/post-card.js
@@ -3,13 +3,13 @@ import { Link } from 'gatsby';
 import LazyImage from '../../common/LazyImage';
 
 const PostCard = ({ post }) => {
-  const { excerpt, heroImage, path, place, regionHierarchy } = post;
+  const { date, excerpt, heroImage, path, place, regionHierarchy } = post;
   return (
     <div className="group">
       <Link className="no-underline text-grey-darkest" to={`${path}`}>
         <LazyImage relSrc={`/w_600/gallery/${heroImage}`} />
         <div className="group-hover:underline font-sans text-xs pt-3">
-          {regionHierarchy.toUpperCase()}
+          {`${regionHierarchy.toUpperCase()} (${date.split('-')[0]})`}
         </div>
         <h2 className="font-heading font-semibold pt-1 text-2xl">{place}</h2>
         <p className="leading-normal pt-1">{excerpt}</p>
