76fd448ba69aebc46f6680dd1459826483354031
kzmeyao <kzmeyao@gmail.com>
2021-05-15 18:01:58 -0700

    confusing backticks

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -9,12 +9,12 @@ const About = () => (
     <h2 className={headingClass}>About the website:</h2>
     <p className={pClass}>
       On my first trip to Japan, I ended up joining the cult of the humble egg
-      salad sando after just one bite. For the uninitiated, `sando` is short for
-      sandwich, and the egg salad sando is the perfect sandwich. Its simplicity,
-      taste, and value just can't be beat. It's probably not even a stretch to
-      say that this bite cemented my love for travel. So here I am, creating
-      this travel journal, and I can't think of a better name for it than to
-      name it after the thing that started it all.
+      salad sando after just one bite. For the uninitiated, <em>sando</em> is
+      short for sandwich, and the egg salad sando is the perfect sandwich. Its
+      simplicity, taste, and value just can't be beat. It's probably not even a
+      stretch to say that this bite cemented my love for travel. So here I am,
+      creating this travel journal, and I can't think of a better name for it
+      than to name it after the thing that started it all.
     </p>
     <br />
     <h2 className={headingClass}>About the author:</h2>
@@ -24,8 +24,8 @@ const About = () => (
       end up spending a lot of time immersing myself in the subject and creating
       something out of it. Although these mini projects tend to change
       frequently, one constant creative outlet for me has been photography.
-      Hence, I created `sando` to document and showcase these journeys. I hope
-      you enjoy your visit.
+      Hence, I created <em>sando</em> to document and showcase these journeys. I
+      hope you enjoy your visit.
     </p>
   </>
 );
