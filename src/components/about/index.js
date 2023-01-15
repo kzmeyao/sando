6b41cb759de06fb4d3ec888c2a255eaf2e45843e
kzmeyao <kzmeyao@gmail.com>
2023-01-15 13:31:15 -0800

    about edits

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -12,20 +12,20 @@ const About = () => (
       salad <em>sando</em> after just one bite. For the uninitiated,{' '}
       <em>sando</em> is short for sandwich, and the egg salad sando is the
       perfect sandwich. Its simplicity, taste, and value just can't be beat.
-      It's probably not even a stretch to say that this bite cemented my love
-      for travel. So here I am, creating this travel journal, and I can't think
-      of a better name for it than to name it after the thing that started it
-      all.
+      It's not even a stretch to say that this bite cemented my love for travel.
+      So here I am, creating this travel journal, and I can't think of a better
+      name for it than to name it after the thing that started it all.
     </p>
     <h2 className={headingClass}>About the author:</h2>
     <p className={pClass}>
-      Hello, my name is Kevin Yao. I'm a software developer by trade, but I'm
-      also a serial creator. My curiosity often leads me down rabbitholes and I
-      end up immersing myself in the various subjects and creating something out
-      of it. Although the projects I dabble in tend to change frequently, one
-      constant creative outlet for me has been travel photography. Hence, I
-      created <em>sando</em> to document and showcase these journeys. I hope you
-      enjoy your visit.
+      Hi, my name is Kevin Yao. I'm a software developer by trade, but I'm also
+      a serial creator. My curiosity often takes me down rabbit holes and that
+      has led me to accumulate an eclectic range of interests over time.
+      Although the projects I dabble in tend to change frequently, one constant
+      creative outlet for me has been photography. I pursue a documentary-like
+      style of photography, hoping to capture scenes that will bring me back to
+      how I remember them. I hope you get something out of them as well. Thanks
+      for visiting!
     </p>
     <h2 className={headingClass}>Want more?</h2>
     <p className={pClass}>
